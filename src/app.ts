fb286df4ef5a11efd2a0ccc86d173c9561792003
Cody Zornes <cody@zornes.dev>
2021-08-19 13:04:28 -0700

    rework app to use synchronous readline

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
 import * as fs from 'fs';
-import * as readline from 'readline';
-import {
-  google,
-  sheets_v4
-} from 'googleapis';
+import * as readlineSync from 'readline-sync';
+import { google, sheets_v4 } from 'googleapis';
 import { AuthPlus } from 'googleapis-common';
 import { OAuth2Client, OAuth2ClientOptions } from 'google-auth-library';
+import { GetTokenResponse } from 'google-auth-library/build/src/auth/oauth2client';
 
 // If modifying these scopes, delete token.json.
 const SCOPES: string[] = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
@@ -14,13 +12,29 @@ const TOKEN_PATH: string = 'token.json';
 const sheets: sheets_v4.Sheets = google.sheets('v4');
 const auth: AuthPlus = google.auth;
 
-
+const authOptions: OAuth2ClientOptions = getAuthOptions();
+const authClient: OAuth2Client = new OAuth2Client(authOptions);
 
 async function main () {
-  // Get a new auth client
-  const authClient = getAuthorizedClient();
+  // Get a new auth client, send the request with the authorized client
+  authorize()
+    .then(() => {
+      sendRequest(buildRequest())
+        .then(handleResponse);
+    });
+}
+main();
 
-  // Build our request
+function handleResponse(response: sheets_v4.Schema$BatchGetValuesResponse) {
+  console.log(response);
+}
+
+async function sendRequest(request) : Promise<sheets_v4.Params$Resource$Spreadsheets$Values$Batchget> {
+  return (await sheets.spreadsheets.values.batchGet(request)).data;
+}
+
+function buildRequest() : sheets_v4.Params$Resource$Spreadsheets$Values$Batchget {
+  // Build our authorized request
   const request: sheets_v4.Params$Resource$Spreadsheets$Values$Batchget = {
     spreadsheetId: '10jSMRIG9nwRppaXE7Hi8VHVRZe8Cd3p2mBN7ZdfJ_RQ',
     ranges: [
@@ -30,72 +44,44 @@ async function main () {
       'Anxieties!A2:C',
     ],
     auth: authClient,
-  };
-
-  // Try to send our request
-  try {
-    const response: sheets_v4.Schema$BatchGetValuesResponse = (await sheets.spreadsheets.values.batchGet(request)).data;
-
-    // do some stuff with the response
-    console.log(response.valueRanges[0].values[0]);
-
-  } catch (err) {
-    console.error(err);
   }
+  return request;
 }
-main();
 
-function getAuthorizedClient() {
-  let authOptions: OAuth2ClientOptions = getAuthOptions();
-
-  const authClient: OAuth2Client = new auth.OAuth2(
-    authOptions.clientId,
-    authOptions.clientSecret,
-    authOptions.redirectUri,
-  );
-  
-  // Try to get existing token from file
-  let authToken;
+async function authorize() {
+  // Try to get existing authorization token from file
   try {
-    authToken = JSON.parse(fs.readFileSync('token.json').toString());
+    let authToken = JSON.parse(fs.readFileSync('token.json').toString());
+    authClient.setCredentials(authToken);
   } catch (err) {
     // Try to get a new token
-    authToken = getNewToken(authClient);
+    authClient.setCredentials((await getNewToken()).tokens);
   }
-  
-  authClient.setCredentials(authToken);
-
-  return authClient;
 }
 
-function getNewToken(authClient: OAuth2Client) : OAuth2Client {
+async function getNewToken() : Promise<GetTokenResponse> {
+  
   const authUrl: string = authClient.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
   });
+
   console.log(`Authorize this app by visiting this URL: ${authUrl}`);
 
-  const rl: readline.Interface = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+  let userCode: string = readlineSync.question(`Enter the code here:`);
 
-  rl.question(`Enter the code from that page: `, (code) => {
-    rl.close();
-    authClient.getToken(code, (err, token) => {
-      if (err) return console.error('Error while trying to retrieve access token', err);
-      authClient.setCredentials(token);
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
+  return authClient.getToken(userCode)
+    .then((tokenResponse) => {
+      //TODO: store token in file 'token.json'
+      return tokenResponse;
+    })
+    .catch((err) => {
+      throw new Error(err);
     });
-  });
-  return authClient;
 }
 
 function getAuthOptions() {
-  let credentials;
+  let credentials: any;
   try {
     credentials = JSON.parse(fs.readFileSync('credentials.json').toString());
   } catch (err) {
